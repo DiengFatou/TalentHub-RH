ecfc26c3033ad8ded45a9108558b08cad92fcb8d
Fatou <dchifai8@gmail.com>
2026-08-14 17:43:01 +0000

    implementation du dashboard candidat

--- app_candidatures/urls.py
+++ app_candidatures/urls.py
@@ -1,11 +1,31 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CandidatureViewSet, DocumentViewSet, EntretienViewSet
+
+from .views import (
+    CandidatureViewSet,
+    DocumentViewSet,
+    EntretienViewSet,
+)
 
 router = DefaultRouter()
-router.register(r'candidatures', CandidatureViewSet, basename='candidatures')
-router.register(r'documents', DocumentViewSet, basename='documents')
-router.register(r'entretiens', EntretienViewSet, basename='entretiens')
+
+router.register(
+    r'candidatures',
+    CandidatureViewSet,
+    basename='candidatures'
+)
+
+router.register(
+    r'documents',
+    DocumentViewSet,
+    basename='documents'
+)
+
+router.register(
+    r'entretiens',
+    EntretienViewSet,
+    basename='entretiens'
+)
 
 urlpatterns = [
     path('', include(router.urls)),
--- backend/urls.py
+++ backend/urls.py
@@ -1,16 +1,93 @@
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from app_users.views import ModifierUtilisateurView
+from django.urls import (
+    path,
+    include
+)
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+from app_users.views import CandidatDashboardView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('app_users.urls')), 
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('utilisateur/modifier/', ModifierUtilisateurView.as_view(), name='modifier-utilisateur'),
-    path('api/offres/', include('app_offres.urls')),
-    path('api/candidatures/', include('app_candidatures.urls')),
-    
+
+    # ========================================================
+    # ADMIN DJANGO
+    # ========================================================
+
+    path(
+        'admin/',
+        admin.site.urls
+    ),
+
+
+    # ========================================================
+    # UTILISATEURS
+    # ========================================================
+
+    path(
+        'api/',
+        include('app_users.urls')
+    ),
+
+
+    # ========================================================
+    # JWT
+    # ========================================================
+
+    path(
+        'api/token/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
+
+    path(
+        'api/token/refresh/',
+        TokenRefreshView.as_view(),
+        name='token_refresh'
+    ),
+
+
+    # ========================================================
+    # OFFRES
+    # ========================================================
+
+    path(
+        'api/offres/',
+        include('app_offres.urls')
+    ),
+
+
+    # ========================================================
+    # CANDIDATURES
+    # ========================================================
+
+    path(
+        'api/candidatures/',
+        include('app_candidatures.urls')
+    ),
+
+     path(
+        "api/dashboard/",
+        CandidatDashboardView.as_view(),
+        name="candidat-dashboard"
+    ),
 ]
+
+
+# ============================================================
+# MEDIA
+# ============================================================
+
+if settings.DEBUG:
+
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
